ff91f15c01bc90b9c82e45a594eecbce9df642ad
Ahmed Jadani <ahjadani@student.1337.ma>
2021-11-04 21:54:16 +0100

    strs

---
+++ ft_strjoin.c
@@ -0,0 +1,34 @@
+#include "libft.h"
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+#include<stdio.h>
+int main()
+{
+	char s1[12] = "foo";
+	char s2[12] = "bar";
+	printf("%s",ft_strjoin(s1,s2));
+}
