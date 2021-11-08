d4c3cb19d2373789f509f75783c38f01c6f3aa88
Ahmed Jadani <ahmed4star@gmail.com>
2021-11-08 12:29:44 +0100

    List linked

--- libft.h
+++ libft.h
@@ -58,4 +58,21 @@ int			ft_isupper(int c);
 int			ft_islower(int c);
 int			ft_isspace(int c);
 
+// bonus //
+typedef struct	s_list
+{
+	void	*content;
+	struct s_list	*next;
+}			t_list;
+t_list		*ft_lstnew(void	*content);
+void		ft_lstadd_front(t_list **lst, t_list *new);
+int			ft_lstsize(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list **lst, void (*del)(void *));
+void		ft_lstister(t_list *lst, void(*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+
 #endif
