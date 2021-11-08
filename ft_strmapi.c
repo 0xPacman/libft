3999793f58c153ce3aa5f98f001942bd6ea2c1d4
Ahmed Jadani <ahmed4star@gmail.com>
2021-11-08 10:15:48 +0100

    function pointers

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -11,9 +11,43 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
 {
-	
+	char	*str;
+	unsigned int	i;
+
+	if (!s || !f)
+		return (NULL);
+	str = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char	mapi_test(unsigned int i, char c)
+{
+	return (c + i);
+}
+
+int main()
+{
+	char	*src;
+	char	*dst;
+
+	src = strdup("12345");
+	dst = ft_strmapi(src, mapi_test);
+	printf("%s",dst);
+	free(src); src = NULL;
 }
 */
