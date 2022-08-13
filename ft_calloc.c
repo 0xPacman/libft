acbdc7f088bff1facbc4fbc5c9cf56ec1ceb512d
Ahmed Jadani <ahmed4star@gmail.com>
2022-08-14 00:20:25 +0100

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	ptr = (void *)malloc(count * size);
+	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
 	ft_bzero(ptr, count * size);
